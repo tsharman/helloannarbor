f2a6776a533e71bb3730217ee6aca9ae1a7ba31b
Torehan Sharman <torehan7@gmail.com>
2012-12-05 02:16:44 +0000

    404 page

--- annarbor/models.py
+++ annarbor/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
+import uuid
+import os
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('profiles', filename)
+
 class Creative(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     link = models.URLField()
-    photo  = models.ImageField(upload_to = 'profiles/')
+    photo  = models.ImageField(upload_to = get_file_path)
     approved = models.BooleanField(default=False, blank=True)
 
     def __unicode__(self):
         return self.first_name + ' ' + self.last_name
+
 
 class Tag(models.Model):
     creative = models.ForeignKey(Creative, related_name="tags")
